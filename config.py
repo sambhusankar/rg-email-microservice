5a9b35faac17ab76fcb0c84404cb7f2393647864
sambhusankar <sambhusankarswain@gmail.com>
2026-08-18 12:14:06 +0530

    feat: attached name with email address

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 
 RESEND_API_KEY = os.environ["RESEND_API_KEY"]
 FROM_EMAIL = os.environ["FROM_EMAIL"]
+FROM_NAME = os.environ.get("FROM_NAME", "RoomGrub")
 
 STREAM_NAME = "rg:emails"
 CONSUMER_GROUP = "email-workers"
--- services/sender.py
+++ services/sender.py
@@ -1,13 +1,13 @@
 import resend
 
-from config import RESEND_API_KEY, FROM_EMAIL
+from config import RESEND_API_KEY, FROM_EMAIL, FROM_NAME
 
 resend.api_key = RESEND_API_KEY
 
 
 def send_email(to, subject, html_body):
     resend.Emails.send({
-        "from": FROM_EMAIL,
+        "from": f"{FROM_NAME} <{FROM_EMAIL}>",
         "to": to,
         "subject": subject,
         "html": html_body,
